736b55df2a124262833e845a04222500c91735e1
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2021-04-13 20:33:38 +0900

    Add vaidationSchema in ConfigModule by Joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restraurants/restraunt.module';
-
+import * as Joi from 'joi';
 
 
 
@@ -12,14 +12,26 @@ import { RestaurantsModule } from './restraurants/restraunt.module';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod', //배포할때는 환경변수파일 무시하겠다.
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('dev', 'prod')
+          .required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      })
+
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '1234',
-      database: 'nuber-eats',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       synchronize: true,
       logging: true,
     }),
