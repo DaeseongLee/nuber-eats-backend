44f777d0f2418af3eaf52e59ba8195a22dab6a76
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2021-04-14 13:22:16 +0900

    Add UserModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restraurants/restraunt.module';
 import * as Joi from 'joi';
 import { Restaurant } from './restraurants/entities/restaurant.entity';
+import { UsersModule } from './users/users.module';
+import { CommonModule } from './common/common.module';
+import { User } from './users/entities/user.entity';
 
 
 
@@ -35,12 +38,13 @@ import { Restaurant } from './restraurants/entities/restaurant.entity';
       database: process.env.DB_NAME,
       synchronize: process.env.NODE_ENV !== 'prod',
       logging: process.env.NODE_ENV !== 'prod',
-      entities: [Restaurant],
+      entities: [User],
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
-    RestaurantsModule,
+    UsersModule,
+    CommonModule,
   ],
   controllers: [],
   providers: [],
