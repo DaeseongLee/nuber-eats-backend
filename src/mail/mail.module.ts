5265bb6fd4cd1b7a5755d8ca6b140f338edfdb13
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2021-04-16 14:47:25 +0900

    Add email Service

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,3 +1,4 @@
+import { MailService } from './mail.service';
 import { DynamicModule, Module } from '@nestjs/common';
 import { CONFIG_OPTIONS } from 'src/common/common.constant';
 import { MailModuleOptions } from './mail.interfaces';
@@ -12,8 +13,9 @@ export class MailModule {
                     provide: CONFIG_OPTIONS,
                     useValue: options,
                 },
+                MailService,
             ],
-            exports: [],
+            exports: [MailService],
         }
     }
 }
