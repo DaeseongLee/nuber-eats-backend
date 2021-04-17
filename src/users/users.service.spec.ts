7b97974a45c8bf79c402318df36248aca0a23112
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2021-04-17 14:09:24 +0900

    Add MockRepository for User Unit Test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,3 +1,4 @@
+import { Repository } from 'typeorm';
 import { MailService } from './../mail/mail.service';
 import { JwtService } from './../jwt/jwt.service';
 import { Verification } from './entities/verification.entity';
@@ -8,7 +9,7 @@ import { UserService } from './users.service';
 
 describe('UserService', () => {
     let service: UserService;
-
+    let usersRepository: MockRepository<User>
     const mockRepository = {
         findOne: jest.fn(),
         save: jest.fn(),
@@ -23,6 +24,8 @@ describe('UserService', () => {
     const mockMailService = {
         sendVerificationEmail: jest.fn(),
     }
+
+    type MockRepository<T> = Partial<Record<keyof Repository<T>, jest.Mock>>;
     beforeAll(async () => {
         const module = await Test.createTestingModule({
             providers: [
@@ -46,12 +49,18 @@ describe('UserService', () => {
             ],
         }).compile();
         service = module.get<UserService>(UserService);
+        usersRepository = module.get(getRepositoryToken(User));
+
     })
 
     it('should be defined', () => {
         expect(service).toBeDefined();
     });
-    it.todo('createAccount');
+
+    describe('createAccount', () => {
+        it('should fail if user exists', () => { });
+    })
+
     it.todo('login');
     it.todo('findById');
     it.todo('editProfile');
