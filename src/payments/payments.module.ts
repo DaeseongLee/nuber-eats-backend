4df014296ccd783085561256ed6bebdba0e5e06b
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2021-04-23 10:06:36 +0900

    Add CreatePayment

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,9 +1,12 @@
+import { PaymentResolver } from './payments.resolver';
+import { Restaurant } from './../restraurants/entities/restaurant.entity';
 import { Payment } from './entities/payment.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { PaymentService } from './payments.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Payment])],
-    providers: []
+    imports: [TypeOrmModule.forFeature([Payment, Restaurant])],
+    providers: [PaymentService, PaymentResolver]
 })
 export class PaymentsModule { }
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,9 +1,48 @@
+import { User } from 'src/users/entities/user.entity';
+import { Restaurant } from './../restraurants/entities/restaurant.entity';
 import { Payment } from './entities/payment.entity';
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
+import { CreatePaymentInput, CreatePaymentOutput } from './dtos/create-payment.dto';
 
 @Injectable()
 export class PaymentService {
-    constructor(@InjectRepository(Payment) private readonly payments: Repository<Payment>) { }
+    constructor(@InjectRepository(Payment) private readonly payments: Repository<Payment>,
+        @InjectRepository(Restaurant) private readonly restaurants: Repository<Restaurant>
+    ) { }
+
+    async createPayment(owner: User, { transactionId, restaurantId }: CreatePaymentInput): Promise<CreatePaymentOutput> {
+        try {
+            const restaurant = await this.restaurants.findOne(restaurantId);
+            if (!restaurant) {
+                return {
+                    ok: false,
+                    error: 'Restaurant not found',
+                };
+            };
+            if (restaurant.ownerId !== owner.id) {
+                return {
+                    ok: false,
+                    error: 'You are not allowed to do this.',
+                };
+            };
+            await this.payments.save(
+                this.payments.create({
+                    transactionId,
+                    user: owner,
+                    restaurant,
+                })
+            );
+            return {
+                ok: true,
+            };
+
+        } catch (error) {
+            return {
+                ok: false,
+                error: 'Could not create payment.',
+            }
+        }
+    }
 }
